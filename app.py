7fe426f325052bf1d3cc0af65c9e51703d25333e
omar zaki <omarzekoo1234@gmail.com>
2024-10-21 05:18:03 +0300

    edit app

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return "<h1>ked kolo tmm yana ya anta!</h1>"
+    return "<h1>fo2 tt7t ymen shmal</h1>"
 
 if __name__ == "__main__":
     from waitress import serve
